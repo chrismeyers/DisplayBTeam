1e4b0db7930489b8806c6ae4b2c7e8f37191067c
William Reeder <wreeder1@gmail.com>
2016-12-17 00:31:26 -0500

    updated search results tab

--- ArTech/src/com/missionse/codeathon/SearchResultPanel.java
+++ ArTech/src/com/missionse/codeathon/SearchResultPanel.java
@@ -24,19 +24,29 @@ public class SearchResultPanel extends JPanel {
 		lon_lbl.setHorizontalAlignment(JTextField.RIGHT);
 		
 		blank_lbl = new JLabel("");
+		blank1_lbl = new JLabel("");
+		blank2_lbl = new JLabel("");
 		
 		lat_txt = new JTextField();
 		lon_txt = new JTextField();
 		
 		set_last_pos = new JToggleButton("Capture search position");
+		save_btn = new JToggleButton("Save");
+		reset_btn = new JToggleButton("Reset");
 		
 		panel.add(lat_lbl);
 		panel.add(lat_txt);
 		panel.add(lon_lbl);
 		panel.add(lon_txt);
 	
-		panel.add(blank_lbl);		
+		panel.add(blank_lbl);
 		panel.add(set_last_pos);
+		
+		panel.add(blank1_lbl);	
+		panel.add(blank2_lbl);	
+		
+		panel.add(save_btn);
+		panel.add(reset_btn);
 		
 		add(panel);
 		
@@ -56,6 +66,14 @@ public class SearchResultPanel extends JPanel {
 						set_last_pos.setSelected(false);
 					}
 				}
+            	else if (reset_btn.isSelected()) {
+            		resetTab();
+            	}
+            	else if (save_btn.isSelected()) {
+            		//TODO
+            		
+            		resetTab();
+            	}
             }
 
             public void mousePressed(MouseEvent mouseEvent)
@@ -68,10 +86,21 @@ public class SearchResultPanel extends JPanel {
         });
 	}
 	
+	private void resetTab() {
+		lat_txt.setText("");
+		lon_txt.setText("");
+		set_last_pos.setSelected(false);
+	}
+	
 	JLabel blank_lbl;
+	JLabel blank1_lbl;
+	JLabel blank2_lbl;
+	
 	JLabel lat_lbl;
 	JLabel lon_lbl;
 	JTextField lat_txt;
 	JTextField lon_txt;	
 	JToggleButton set_last_pos;
+	JToggleButton save_btn;
+	JToggleButton reset_btn;
 }
